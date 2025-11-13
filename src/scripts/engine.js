5995355aefffdf6bfa75d357435c4682997d4006
Hugodz <hugosilva1ads@gmail.com>
2025-11-13 16:00:37 -0300

    joguinho detona ralpah

---
+++ src/scripts/engine.js
@@ -0,0 +1,91 @@
+
+const state = {
+view:{
+    timer:document.querySelector("#time-left"),
+    score:document.querySelector("#score"),
+    enemy:document.querySelector(".enemy"),
+    square:document.querySelectorAll(".square")
+},
+
+values:{
+    timerId : null,
+    countDownTimerId: setInterval(countDown, 1000),
+    timervelocity :1000,
+    hitPosition : 0,
+    result : 0,
+    currentTime : 60,
+}
+};
+
+function countDown (){
+    state.values.currentTime--;
+    state.view.timer.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0){
+        clearInterval(state.values.countDownTimerId)
+        clearInterval(state.values.timerId)
+
+        alert(`Tempo acabou moral, você fez ${state.values.result} pontos`)
+    }
+
+
+}
+
+
+function playSound (){
+    let audio = new Audio ("./src/audios/hit.m4a");
+    audio.volume =0.2;
+    audio.play()}
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare,state.values.timervelocity)
+}
+
+
+function randomSquare (){
+    state.view.square.forEach((square)=>{
+        square.classList.remove("enemy")
+        
+    });
+    let randomNumber = Math.floor(Math.random() * 9);
+
+    let randomSquare = state.view.square[randomNumber];
+    randomSquare.classList.add("enemy");
+
+    state.values.hitPosition = randomSquare.id;
+
+
+}
+
+
+
+function addListenerHitBox(){
+    state.view.square.forEach((square)=>{
+        square.addEventListener("mousedown",() => {
+            if(square.id === state.values.hitPosition){
+              
+            state.values.result++;
+            state.view.score.textContent = state.values.result;
+            state.values.hitPosition =null;
+            playSound()
+            }
+
+        }
+        )
+
+    });
+
+
+}
+function playSound (){
+    let audio = new Audio ("./src/audios/hit.m4a");
+    audio.volume =0.2;
+    audio.play()
+}
+function init (){
+    
+    moveEnemy();
+    addListenerHitBox();
+
+}
+
+init()
